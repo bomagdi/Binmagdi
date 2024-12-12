55d10321aa0651123630ee46533a07379859f389
bomagdi <samyy.ads1@gmail.com>
2024-12-12 11:41:47 +0300

    contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,29 +1,56 @@
-import React from "react";
+import { MailIcon, HomeIcon, PhoneCall } from "lucide-react";
+// Components
+import Form from "@/components/Form";
+
 
 const Contact = () => {
   return (
-    <div>
-      <div className="grid h-screen place-content-center bg-white px-4">
-        <div className="text-center">
-          <h1 className="text-6xl font-black text-gray-200">
-            Something is cooking up
-          </h1>
+    <section>
+      <div className="container mx-auto px-8">
+        {/* Text & Illustration */}
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
+          {/* Text */}
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+              <span className="w-[30px] h-[2px] bg-primary"></span>
+              Say Hello 👋🏻
+            </div>
+            <h1 className="h1 max-w-md mb-8">Let's Work Together.</h1>
+            <p className="subtitle max-w-[400px] ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius,
+              est.
+            </p>
+          </div>
+          {/* Illustration */}
+          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat"></div>
+        </div>
+        {/* Info text & Form */}
+        <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+          {/* Info text */}
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+            {/* Adress */}
+            <div className="flex items-center gap-x-8">
+              <HomeIcon size={18} className="text-primary " />
+              <div className="capitalize">
+                Al Nairyah, eastern province, saudi arabia
+              </div>
+            </div>
+            {/* Mail */}
+            <div className="flex items-center gap-x-8">
+              <MailIcon size={18} className="text-primary" />
+              <div>binmagdi97@gmail.com</div>
+            </div>
 
-          <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Uh-oh!
-          </p>
-
-          <p className="mt-4 text-gray-500">We can't find that page.</p>
-
-          <a
-            href="/"
-            className="mt-6 inline-block rounded bg-primary px-5 py-3 text-sm font-medium text-white hover:bg-secondary focus:outline-none focus:ring"
-          >
-            Go Back Home
-          </a>
+            {/* Phone */}
+            <div className="flex items-center gap-x-8">
+              <PhoneCall size={18} className="text-primary" />
+              <div>+966 558 147 884</div>
+            </div>
+          </div>
+          <Form />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
